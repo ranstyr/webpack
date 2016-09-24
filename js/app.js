a75e71fb580a39e0d27ef0f81b4a111212d0a965
ran.styr <ran.styr@gmail.com>
2016-09-24 03:24:21 +0300

    css

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ document.write("  welcome to app.js 8888");
 
 console.log("app loaded");
 
+var img = document.createElement('img');
+img.style.height = '25%';
+img.style.width = '25%';
+img.src = require('../images/webpack_logo.png');
+
+document.getElementById('img_container').appendChild(img);
+
+require('../css/app.css');
--- webpack.config.js
+++ webpack.config.js
@@ -6,34 +6,43 @@ var path = require("path");
 
 module.exports = {
     //relative root directory for entry key
-    context : path.resolve("js"),
+    context: path.resolve("js"),
     //entry - name of set of files that we want to include n the build
-    entry : ["./app" ,'./utils'] ,
+    entry: ["./app"],
     //output file - can have a few key , we care about the file name
-    output : {
+    output: {
         //put the output file/bubdle.js at build/js
         //We dont want to build to public folder as we dont want to add it to source code
-        path : path.resolve('./build/js'),
+        path: path.resolve('./build/'),
         //tell webpack from where to serve the output file at the webserver
         //every request from /public/assest/js will go to /build/js
-        publicPath : '/public/assest/js',
+        publicPath: '/public/assest/',
         filename: "./bundle.js"
     },
-    module : {
-        loaders : [
+    module: {
+        loaders: [
             {
-              test: /\.es6$/,
-              excelude: /node_modules/,
-              loader : "babel-loader"
+                test: /\.css$/,
+                excelude: /node_modules/,
+                loader: "style-loader!css-loader"
+            },
+            //? -> ? is paramter for the loader
+            //any image under 1000000 (100kb) is going to inline base 64
+            //usally we will want to set limit to 8kb
+            //any bigger will be created as seperate image and we will request it in a diffrent request
+            {
+                test: /\.(png|jpg)$/,
+                excelude: /node_modules/,
+                loader: "url-loader?limit=1000000"
             }
         ]
     },
     //request from the root will go to public
-    devServer : {
-      contentBase : 'public'
+    devServer: {
+        contentBase: 'public'
     },
-    watch : true,
-    resolve : {
-        extensions : ['' , '.js' , '.es6']
+    watch: true,
+    resolve: {
+        extensions: ['', '.js', 'css']
     }
 };
